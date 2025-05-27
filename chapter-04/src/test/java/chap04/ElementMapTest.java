c174a5309bebfd962d77b79e564900c4380b2a7e
Moonsoo <aptx15hibr@naver.com>
2025-05-28 08:10:07 +0900

    feat) 스트림 집계함수 실습

--- chapter-04/src/test/java/chap04/ElementMapTest.java
+++ chapter-04/src/test/java/chap04/ElementMapTest.java
@@ -28,4 +28,24 @@ public class ElementMapTest {
                 .collectSortedList(Comparator.reverseOrder())
                 .subscribe(System.out::println);
     }
+
+    @Test
+    public void p_149(){
+        Flux.just(3,5,7,9,11,15,16,17)
+                .any(e->e%2==0)
+                .subscribe(hasEvens -> log.info("Has evens: {}", hasEvens));
+    }
+
+    @Test
+    public void p_150_1(){
+        Flux.range(1, 5)
+                .reduce(0,(acc,elem)-> acc+elem)
+                .subscribe(result-> log.info("Result: {}",result));
+    }
+    @Test
+    public void p_150_2(){
+        Flux.range(1,5)
+                .scan(0,(acc,elem)->acc+elem)
+                .subscribe(result->log.info("Result : {}",result));
+    }
 }
